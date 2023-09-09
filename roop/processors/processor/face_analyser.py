1f37f6152c5e4fb0738008c4fc7e17e4cd6d5e12
zhaojigang <1197596604@qq.com>
2023-09-09 20:44:11 +0800

    xxx

--- roop/processors/processor/face_analyser.py
+++ roop/processors/processor/face_analyser.py
@@ -13,7 +13,7 @@ def get_face_analyser():
 
     with THREAD_LOCK:
         if FACE_ANALYSER is None:
-            root = resolve_relative_path('../../models')
+            root = resolve_relative_path('../../')
             # cpu:CPUExecutionProvider  gpu:CUDAExecutionProvider
             FACE_ANALYSER = insightface.app.FaceAnalysis(name='buffalo_l', root=root, providers='CUDAExecutionProvider')
             FACE_ANALYSER.prepare(ctx_id=0)
